8309e666d9a07a3781684b70c0ac709d1a5fe6b6
hei <izzie0082005@gmail.com>
2022-12-21 00:19:16 +0000

    beta

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  @Input() inputData:any;
+  
   constructor() { }
 
   ngOnInit(): void {
